3057661da6152417493230459e0db837dd5496fb
Kettel <75881317+Kettel1@users.noreply.github.com>
2022-01-03 13:22:56 +0300

    - Поправил скролл в блоке BurgerIngredients.jsx

--- src/compontents/BurgerIngredients/BurgerIngredients.jsx
+++ src/compontents/BurgerIngredients/BurgerIngredients.jsx
@@ -1,22 +1,40 @@
 import {Tab} from '@ya.praktikum/react-developer-burger-ui-components';
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import BurgerIngredientsStyles from './BurgerIngredients.module.scss'
 import RenderBurgerIngredients from '../RenderBurgerIngredients/RenderBurgerIngredients';
 import {useSelector} from "react-redux";
-
+import {debounce} from "../../utils/helpers";
 
 // @ts-ignore
 const BurgerIngredients = () => {
-    const [currentTab, setCurrentTab] = React.useState('one')
+    const [currentTab, setCurrentTab] = useState('one')
     const {ingredients} = useSelector((state => state.ingredients))
+
     const bunRef = useRef(null)
     const sauceRef = useRef(null)
     const mainRef = useRef(null)
+    const scrollRef = useRef(null)
 
     const scrollToRef = (ref) => {
         ref.current.scrollIntoView({
             behavior: "smooth"
         })
+    }
+
+    const setCurrentTabFromHeight = () => {
+        const currentHeight = scrollRef.current.scrollTop
+
+        if (currentHeight < 300) {
+            setCurrentTab('one')
+        }
+
+        if (currentHeight >= 290 && currentHeight <= 800) {
+            setCurrentTab('two')
+        }
+
+        if (currentHeight > 790) {
+            setCurrentTab('three')
+        }
     }
 
     function ingredientsTabs() {
@@ -48,7 +66,10 @@ const BurgerIngredients = () => {
         <section className={BurgerIngredientsStyles.container}>
             <h1 className={BurgerIngredientsStyles.title}>Соберите бургер</h1>
             {ingredientsTabs()}
-            <div className={BurgerIngredientsStyles.scrollBar}>
+            <div className={BurgerIngredientsStyles.scrollBar}
+                 ref={scrollRef}
+                 onScroll={debounce(setCurrentTabFromHeight, 100)}
+            >
                 <RenderBurgerIngredients data={ingredients} type={'bun'} ref={bunRef}/>
                 <RenderBurgerIngredients data={ingredients} type={'sauce'} ref={sauceRef}/>
                 <RenderBurgerIngredients data={ingredients} type={'main'} ref={mainRef}/>
